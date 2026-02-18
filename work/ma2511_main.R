c5a1532b1e711f87253d4eee08b0089efe807ad8
owner <owner@queef.party>
2026-02-18 17:12:40 +0000

    refactor: split main R preprocessing into reusable functions
    Add reusable load and feature-building functions and prepare wide feature tables for both R1 and R3 datasets.

--- work/ma2511_main.R
+++ work/ma2511_main.R
@@ -10,28 +10,34 @@ library(arrow)
 
 # 3. Load Data
 
-inputPath <- file.path("output", "preprocessed_raw_long.parquet")
+BuildInputPath <- function(ratId) {
+  file.path("output", paste0("preprocessed_raw_long_", ratId, ".parquet"))
+}
 
-rawLong <- read_parquet(inputPath)
-rawLong <- as.data.frame(rawLong)
+LoadRawLong <- function(ratId) {
+  inputPath <- BuildInputPath(ratId)
+  rawLong <- read_parquet(inputPath)
+  as.data.frame(rawLong)
+}
 
 # 4. Data Cleaning and Preprocessing
 
-windowFeaturesLong <- aggregate(
-  value ~ state + epoch + channel,
-  data = rawLong,
-  FUN = function(x) c(
-    mean = mean(x),
-    sd = sd(x),
-    var = var(x),
-    rms = sqrt(mean(x^2))
+BuildWindowFeaturesLong <- function(rawLong) {
+  windowFeaturesLong <- aggregate(
+    value ~ state + epoch + channel,
+    data = rawLong,
+    FUN = function(x) c(
+      mean = mean(x),
+      sd = sd(x),
+      var = var(x),
+      rms = sqrt(mean(x^2))
+    )
   )
-)
-
-windowFeaturesLong <- cbind(
-  windowFeaturesLong[c("state", "epoch", "channel")],
-  as.data.frame(windowFeaturesLong$value)
-)
+  cbind(
+    windowFeaturesLong[c("state", "epoch", "channel")],
+    as.data.frame(windowFeaturesLong$value)
+  )
+}
 
 MakeWideByFeature <- function(data, featureName) {
   longFeature <- data[c("state", "epoch", "channel", featureName)]
@@ -51,24 +57,34 @@ MakeWideByFeature <- function(data, featureName) {
   wideFeature
 }
 
-meanWide <- MakeWideByFeature(windowFeaturesLong, "mean")
-sdWide <- MakeWideByFeature(windowFeaturesLong, "sd")
-varWide <- MakeWideByFeature(windowFeaturesLong, "var")
-rmsWide <- MakeWideByFeature(windowFeaturesLong, "rms")
+BuildWindowFeaturesWide <- function(rawLong) {
+  windowFeaturesLong <- BuildWindowFeaturesLong(rawLong)
+  meanWide <- MakeWideByFeature(windowFeaturesLong, "mean")
+  sdWide <- MakeWideByFeature(windowFeaturesLong, "sd")
+  varWide <- MakeWideByFeature(windowFeaturesLong, "var")
+  rmsWide <- MakeWideByFeature(windowFeaturesLong, "rms")
 
-windowFeaturesWide <- Reduce(
-  function(left, right) merge(left, right, by = c("state", "epoch")),
-  list(meanWide, sdWide, varWide, rmsWide)
-)
+  windowFeaturesWide <- Reduce(
+    function(left, right) merge(left, right, by = c("state", "epoch")),
+    list(meanWide, sdWide, varWide, rmsWide)
+  )
+  windowFeaturesWide <- windowFeaturesWide[order(windowFeaturesWide$state, windowFeaturesWide$epoch), ]
+  rownames(windowFeaturesWide) <- NULL
+  windowFeaturesWide
+}
 
-windowFeaturesWide <- windowFeaturesWide[order(windowFeaturesWide$state, windowFeaturesWide$epoch), ]
-rownames(windowFeaturesWide) <- NULL
+rawLongR1 <- LoadRawLong("R1")
+rawLongR3 <- LoadRawLong("R3")
 
-print(dim(windowFeaturesWide))
+windowFeaturesWideR1 <- BuildWindowFeaturesWide(rawLongR1)
+windowFeaturesWideR3 <- BuildWindowFeaturesWide(rawLongR3)
 
-stateLabels <- windowFeaturesWide$state
-featureCols <- setdiff(names(windowFeaturesWide), c("state", "epoch"))
-X <- as.matrix(windowFeaturesWide[, featureCols])
+print(dim(windowFeaturesWideR1))
+print(dim(windowFeaturesWideR3))
+
+stateLabels <- windowFeaturesWideR1$state
+featureCols <- setdiff(names(windowFeaturesWideR1), c("state", "epoch"))
+X <- as.matrix(windowFeaturesWideR1[, featureCols])
 X <- scale(X, center = TRUE, scale = TRUE)
 
 # 6. Linear Algebra Methods
